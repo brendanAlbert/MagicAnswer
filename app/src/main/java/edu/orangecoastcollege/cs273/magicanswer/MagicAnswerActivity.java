1e123d945fe7ead2140ce2aadf4d4c8d24acb763
brendan <alber123@mail.chapman.edu>
2017-11-07 12:51:02 -0800

    Added Javadocs. Reduced size of text for input question.

--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -7,6 +7,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+/**
+ * MagicAnswerActivity is the main controller for our Magic Answer app.
+ *
+ * A new MagicAnswer object is instantiated, View widgets are wired up,
+ * and a SensorManager is obtained to gain access to the accelerometer of
+ * the device running the app.
+ *
+ * When the user tilts the device enough to trigger the sensor, displayMagicAnswer is called
+ * and a random answer is acquired from the MagicAnswer object.
+ *
+ * Another important method is onStop.  When the User switches contexts, or puts this app
+ * in the background, we want to unregister the listener which detects shakes using the
+ * accelerometer.  This saves memory and prevents unnecessary drain on the battery since the User
+ * is not using it.
+ *
+ * If the User returns, onResume is called and a new listener is registered.
+ */
 public class MagicAnswerActivity extends AppCompatActivity {
 
     MagicAnswer magicAnswer;
@@ -17,6 +34,21 @@ public class MagicAnswerActivity extends AppCompatActivity {
     // Create a reference to our ShakeDetector
     private ShakeDetector mShakeDetector;
 
+    /**
+     * onCreate sets the content view and gets a reference to the TextView widget for displaying
+     * a random answer.
+     *
+     * A new MagicAnswer object is instantiated.
+     *
+     * The SensorManager is registered and we identify that we want to use the device's
+     * accelerometer sensor.
+     *
+     * We instantiate a ShakeDetector which if a valid shake is detected, will call
+     * displayMagicAnswer().
+     *
+     * @param savedInstanceState is the state the app may have been in before it was sent
+     *                           to the background and is being returned from.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,18 +71,32 @@ public class MagicAnswerActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * displayMagicAnswer simply accesses a random answer from our MagicAnswer object.
+     * The answerTextView is populated with the answer and displayed.
+     */
     public void displayMagicAnswer()
     {
         String randomAnswer = magicAnswer.getRandomAnswer();
         answerTextView.setText(randomAnswer);
     }
 
+    /**
+     * onResume is called after onStart when the user first starts the app, or it can be
+     * called after the user returns from a different app or activity.  We want to
+     * register a listener to the accelerometer.
+     */
     @Override
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(mShakeDetector, accelerometer, SensorManager.SENSOR_DELAY_UI);
     }
 
+    /**
+     * onStop is called when the activity is finishing or being destroyed by the system.
+     * For instance, if the user switches apps, we don't want this listener to continue listening,
+     * hogging memory and draining the battery.
+     */
     @Override
     protected void onStop() {
         super.onStop();
--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/ShakeDetector.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/ShakeDetector.java
@@ -7,6 +7,22 @@ import android.hardware.SensorManager;
 
 /**
  * Created by balbert on 10/31/2017.
+ *
+ * ShakeDetector implements the SensorEventListener interface.
+ *
+ * We use this class and instances of it to utilize the various sensors available to
+ * the Android OS.
+ *
+ * In this case we want to listen for only accelerometer sensor events
+ * to detect when the User shakes the device, like they would a Magic 8 ball to
+ * trigger a response to their question.
+ *
+ * We tune the sensitivity using the THRESHOLD constant so we can detect when the user
+ * is shaking the device intentionally versus simply moving it slightly.
+ *
+ * We also make sure a certain specified time has elapsed between shakes.  This
+ * is to prevent the detector from being too sensitive; triggering several times
+ * during what is supposed to be a single shake.
  */
 
 public class ShakeDetector implements SensorEventListener {
@@ -19,11 +35,30 @@ public class ShakeDetector implements SensorEventListener {
 
     private OnShakeListener mListener;
 
+    /**
+     * ShakeDetector is a parameterized constructor that accepts a listener to initialize
+     * the OnShakeListener.
+     * @param listener
+     */
     public ShakeDetector(OnShakeListener listener)
     {
         mListener = listener;
     }
 
+    /**
+     * onSensorChanged will detect changes to its sensors all the time, so
+     * we have an if statement to check that the sensor type that is firing
+     * is indeed from the accelerometer.
+     *
+     * We then check if the force of the shake meets our specified THRESHOLD constant.
+     *
+     * If these two conditions are met, we finally check that enough time has
+     * elapsed since the last shake.
+     *
+     * If all three conditions are met, we register a shake event.
+     *
+     * @param sensorEvent when a sensor event is detected and passed in.
+     */
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
@@ -57,14 +92,27 @@ public class ShakeDetector implements SensorEventListener {
         }
     }
 
+    /**
+     * onAccuracyChanged must be implemented because ShakeDetector implements the
+     * SensorEventListener interface.
+     *
+     * @param sensor
+     * @param i
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
         // Do nothing, not being used, but must be defined.
     }
 
-    // Define an interface for others to implement whenever a
-    // true shake occurs. Interface = contract (method declarations WITHOUT implementation)
-    // Some other class has to implement the method.
+
+
+    /**
+     * Define an interface for others to implement whenever a
+     * true shake occurs. Interface = contract (method declarations WITHOUT implementation)
+     * Some other class has to implement the method.  In the case of this app,
+     * in onCreate in MagicAnswerActivity.  onShake is implemented there
+     * and it calls the displayMagicAnswer method.
+     */
     public interface OnShakeListener
     {
         void onShake();
